b095be7918d82aaf569a0ba2cb2e4f6cbcb736cc
Son Nguyen <nyenson1997@gmail.com>
2020-09-02 14:53:51 -0500

    Updates

---
+++ precache-manifest.efcaa45305ee43d12dcb7bf15182f766.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "287b577123fcd4206e1668391ba883de",
+    "url": "/react-blog-github/index.html"
+  },
+  {
+    "revision": "f791a10aeacb6a925753",
+    "url": "/react-blog-github/static/css/main.cfa08aa7.chunk.css"
+  },
+  {
+    "revision": "cf352fcb46f8dc475556",
+    "url": "/react-blog-github/static/js/2.77de8f68.chunk.js"
+  },
+  {
+    "revision": "189e23ef3562d267680aa9086e84695d",
+    "url": "/react-blog-github/static/js/2.77de8f68.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f791a10aeacb6a925753",
+    "url": "/react-blog-github/static/js/main.d927baf7.chunk.js"
+  },
+  {
+    "revision": "8216af6db86cb58257df",
+    "url": "/react-blog-github/static/js/runtime-main.b7e97252.js"
+  }
+]);
