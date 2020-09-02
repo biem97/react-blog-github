6d7cd4a6d2aa5bc469a0efa9466aa447d5f60db0
Son Nguyen <nyenson1997@gmail.com>
2020-09-02 14:11:50 -0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,32 @@
+export const config = {
+  // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
+  githubConvertedToken: "ZWJlZDFmNzc1ZjdjYjMwMGFiY2YxYjc3OWQwNTU2MzNjZDcyYjJlNA==",
+
+  // Your Github UserName
+  githubUserName: "biem97",
+
+  // Your Github Repo Name Where You Have your issues as Blog
+  githubRepo: "react-blog-github",
+
+  // Set it to true if you have a Github app to add to this project
+  // and fill the client ID & secret
+  enableOAuth: true,
+  OAuthClientID: 'Iv1.e72501c449de26af',
+  OAuthSecret: '5e57c318f627a46584e5f9afdbf0e28cfa4553ea',
+
+  // Your Personal Blog Title
+  title : "Son Nguyen" ,
+
+  // Your Personal Blog Subtitle
+  subtitle : "Junior Web Developer",
+
+  // Header customization
+  header: {
+    backgroundColor: '#f1f6f8', // can be a CSS gradient
+    backgroundColorDark: '#4C566A',
+    titleColor: '#ff5252',
+    titleColorDark: '#16a085',
+    subtitleColor: '#37474f',
+    subtitleColorDark: '#D8DEE9',
+  },
+};
